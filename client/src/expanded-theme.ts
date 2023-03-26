54950fcb6d964ef669f2b468a08d81d66d6f15e1
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-03-26 22:33:10 +0530

    MUI configurations and themes setup

---
+++ client/src/expanded-theme.ts
@@ -0,0 +1,11 @@
+//eslint-disable-next-line
+import { Palette, PaletteColor } from "@mui/material";
+
+declare module "@mui/material/styles/createPalette" {
+	interface PaletteColor {
+		[key: number]: string;
+	}
+	interface Palette {
+		tertiary: PaletteColor;
+	}
+}
