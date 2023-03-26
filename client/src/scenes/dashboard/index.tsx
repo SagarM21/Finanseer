eff6fc162d5233d8578e7d2e4a27608bba51f984
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-03-26 23:32:04 +0530

    Dashboard layout

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,12 +1,82 @@
 import { Box } from "@mui/material";
-import { useTheme } from "@mui/material";
-import React from "react";
+import { useMediaQuery } from "@mui/material";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
-type Props = {};
+const gridTemplateLargeScreens = `
+    "a b c"
+    "a b c"
+    "a b c"
+    "a b f"
+    "d e f"
+    "d e f"
+    "d h i"
+    "g h i"
+    "g h j"
+    "g h j"
+`;
 
-const Dashboard = (props: Props) => {
-	const { palette } = useTheme();
-	return <Box color={palette.grey[300]}>Dashboard</Box>;
+const gridTemplateSmallScreens = `
+"a"
+"a"
+"a"
+"a"
+"d"
+"d"
+"d"
+"g"
+"g" 
+"g"
+"b"
+"b"
+"b"
+"b"
+"e"
+"e"
+"h"
+"h"
+"h"
+"h"
+"c"
+"c"
+"c"
+"f"
+"f"
+"f"
+"i"
+"i"
+"j"
+"j"
+`;
+
+const Dashboard = () => {
+	const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
+	return (
+		<Box
+			width='100%'
+			height='100%'
+			display='grid'
+			gap='1.5rem'
+			sx={
+				isAboveMediumScreens
+					? {
+							gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+							gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+							gridTemplateAreas: gridTemplateLargeScreens,
+					  }
+					: {
+							gridAutoColumns: "1fr",
+							gridAutoRows: "80px",
+							gridTemplateAreas: gridTemplateSmallScreens,
+					  }
+			}
+		>
+			<Row1 />
+			<Row2 />
+			<Row3 />
+		</Box>
+	);
 };
 
 export default Dashboard;
