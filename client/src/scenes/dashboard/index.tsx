aa43e6d36c6879984d2a8555bc88811a0ea17ab7
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-03-26 23:01:08 +0530

    import error and dashboard pg setup

---
+++ client/src/scenes/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { useTheme } from "@mui/material";
+import React from "react";
+
+type Props = {};
+
+const Dashboard = (props: Props) => {
+	const { palette } = useTheme();
+	return <Box color={palette.grey[300]}>Dashboard</Box>;
+};
+
+export default Dashboard;
