0f809fa3bf54e12aa65b0b00d845a8fd971a6df5
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-03-27 00:43:26 +0530

    fix: insertion of single data to work on frontend

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import mongoose from "mongoose";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 
 // CONFIGURATIONS
 dotenv.config();
@@ -31,5 +33,9 @@ mongoose
 	})
 	.then(async () => {
 		app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+        // ADD DATA WHEN NEEDED
+		// await mongoose.connection.db.dropDatabase();
+		// KPI.insertMany(kpis);
 	})
 	.catch((error) => console.log(`${error} did not connect.`));
